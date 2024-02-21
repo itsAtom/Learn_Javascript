a571e3652bb35b4fa1c6dab9529a35d5cda6105b
Dhruv Desai <dhruvdesai770@gmail.com>
2024-02-21 18:25:31 -0500

    Done with Variables, Datatypes, Datatype conversion concept

---
+++ 02_datatypes.js
@@ -0,0 +1,39 @@
+// In Javascript, Datatypes are mainly divided into 2 types: Primitive and Non- primitive
+// --Primitive--
+
+//undefined
+let u; // Value is undefined, type is undefined.
+
+//number
+let num1= 369; // Here number can be in decimal as well.
+
+//null
+let n= null;
+
+//string
+let name = "Dhruv";
+
+//symbol
+let s = Symbol("It is a symbol");
+
+//boolean
+let isValid = true;
+
+//bigInt
+let x = BigInt("123456789012345678901234567890");
+
+// --Non-primitive--
+- Object
+// The object datatype contains:
+
+Array
+const cars = ["Audi", "Porsche", "BMW"];
+
+//Object
+const person = {firstName:"Dhruv", lastName:"Desai"};
+
+//Date
+const date = new Date("2024-02-21");
+
+//We can check the type of variable using:
+console.log(typeof(num1));
