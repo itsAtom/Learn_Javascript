dab3a7af60b283849325218d3822e7239119c96c
Dhruv Desai <dhruvdesai770@gmail.com>
2024-02-21 19:21:20 -0500

    Datatype conversion

--- Theory/03_datatype_conversion.js
+++ Theory/03_datatype_conversion.js
@@ -1,6 +1,26 @@
 //If you try to add two strings, the result will be a string concatenation
 let a = 10;
 let b = 20;
-let c = a + b;
-console.log(c)
-console.log(a);
+let c = "30";
+let d = "2";
+let e = "test";
+
+console.log(a + c); //Output: 1020
+
+console.log(a + b + c); //Output: 3030
+
+//Note: The JavaScript interpreter works from left to right.
+//First 10 + 20 is added because x and y are both numbers.
+//Then 30 + "30" is concatenated because z is a string.
+
+console.log(c + b + a); //Output: 302010
+
+//JavaScript will try to convert strings to numbers in all numeric operations except "+" operator which performs String Concatenation.
+console.log(c/d); //Output: 15
+console.log(c*d); //Output: 60
+console.log(c-d); //Output: 28
+
+//If we are trying to perform arithmetic operation with a non-numeric string, result will be NAN.
+console.log(b/e); // Output: NaN
+//Note: Here the type of Nan is number.
+console.log(typeof NaN); // Output: Number
